04d1d7013459e57e13211de3eb6dd88f909674c9
apoorvapendse <apoorvavpendse@gmail.com>
2023-09-30 08:12:35 +0530

    adding tasks to DB

---
+++ src/components/Task.tsx
@@ -0,0 +1,17 @@
+import '../styles/Task.css'
+const Task = () => {
+    return (
+        <div className="task-body">
+            <div className="task-name">
+                <p>Go to sleep</p>
+            </div>
+            <div className="task-options">
+                <p>Mark As Done</p>
+                <p>Edit Task</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Task
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,17 +1,29 @@
 import { User } from 'firebase/auth'
 import '../styles/Todo.css'
-import { FormEvent, useEffect, useRef } from 'react'
-import { addDoc, collection, getDoc, getDocs, getFirestore, query, where } from 'firebase/firestore'
+import { FormEvent, useEffect, useRef, useState } from 'react'
+import { DocumentData, QueryDocumentSnapshot, addDoc, collection, getDoc, getDocs, getFirestore, query, where } from 'firebase/firestore'
 import { app } from '../firebase'
+import Task from './Task'
+import { DocumentReference } from 'firebase/firestore/lite'
 
 interface PropType {
     user: User
 }
+interface Task {
+    name: String;
+    priority: number;
+
+}
+
+
+
+
 
 const firestore = getFirestore(app);
 
 
 const Todo = ({ user }: PropType): JSX.Element => {
+    const [currentUser, setCurrentUser] = useState<DocumentReference<DocumentData, DocumentData> | QueryDocumentSnapshot<DocumentData, DocumentData>>();
 
 
 
@@ -31,15 +43,28 @@ const Todo = ({ user }: PropType): JSX.Element => {
                 })
 
                 console.log("User created successfully:", newUser)
+                setCurrentUser(newUser)
             }
             else {
                 console.log("user already present in firestore:", result.docs[0].data())
+                setCurrentUser(result.docs[0])
             }
         }
         checkUser()
     }, [])
 
-    const taskSubmitHandler = (e: FormEvent) => {
+
+    const updateTasks = async (currID: string) => {
+        const TasksRef = collection(firestore, `Users/${currID}/Tasks`);
+        const TaskDocs = await getDocs(TasksRef);
+        const tasks = TaskDocs.docs;
+        tasks.forEach((item) => {
+            console.log(item.data())
+        })
+
+    }
+
+    const taskSubmitHandler = async (e: FormEvent) => {
         e.preventDefault();
 
         // Access the values using refs
@@ -50,6 +75,21 @@ const Todo = ({ user }: PropType): JSX.Element => {
         console.log('Task Name:', taskName);
         console.log('Task Priority:', taskPriority);
 
+
+        const currID = currentUser?.id;
+        console.log(currID)
+
+        const userReference = collection(firestore, `Users/${currID}/Tasks`);
+        const task: Task = {
+            name: taskName as string,
+            priority: taskPriority != undefined ? +taskPriority : 1,
+        }
+
+        const newTask = await addDoc(userReference, task);
+        console.log("TaskAdded:", newTask);
+
+        if (currID)
+            updateTasks(currID);
         // Clear the form inputs if needed
 
 
@@ -76,13 +116,14 @@ const Todo = ({ user }: PropType): JSX.Element => {
 
             <form className='tasksAdder' onSubmit={taskSubmitHandler} >
                 Enter your task
-                <input type="text" placeholder='task name' ref={taskNameRef} />
-                <input type="number" placeholder='task priority' ref={taskPriorityRef} />
+                <input type="text" placeholder='task name' ref={taskNameRef} required />
+                <input type="number" placeholder='task priority' ref={taskPriorityRef} required />
                 <button type='submit'>Add Task</button>
             </form>
 
             <main className='tasksContainer'>
                 {/* Display tasks here */}
+                <Task />
             </main>
         </div>
     )
